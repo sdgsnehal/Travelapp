fb0e1f986c91f30f0080ce6cd495bf266829f653
sdgsnehal <sdgsnehal@gmail.com>
2023-04-18 18:56:33 +0530

    themeprovider

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
 import List from "./components/List/List";
 import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  // Customize your theme here
+});
 function App() {
   return (
     <>
+   <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
@@ -17,6 +22,9 @@ function App() {
           <Map />
         </Grid>
       </Grid>
+      </ThemeProvider>
+    
+
     </>
   );
 }
---
+++ src/components/Header/styles.js
@@ -0,0 +1,16 @@
+
+import { makeStyles } from "@mui/styles";
+
+
+
+export default makeStyles((theme)=>({
+title:{
+    display:'none',
+    [theme.breakpoints.up('sm')]:{
+        display:'block',
+    }
+   
+}
+}));
+// function to find odd no in array 
+
